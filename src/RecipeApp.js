03a5dc9535520a6999c6fb775bf1b972f210fe7e
Isaias <impomales@gmail.com>
2018-01-14 00:30:51 +0000

    add default recipes and images

--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -3,14 +3,66 @@ import Recipe from './Recipe';
 import './RecipeApp.css';
 
 class RecipeApp extends Component {
+  static defaultProps = {
+    recipes: [
+      {
+        
+        img: "french_toast.jpg",
+        title: "French Toast",
+        ingredients: [
+          '6 thick slices of bread',
+          '2 eggs',
+          '2/3 cup of milk',
+          '1/4 tsp of ground cinnamon',
+          '1/4 tsp of nutmeg',
+          '1 tsp of vanilla extract',
+          'pinch of salt'
+        ],
+        instructions: "Beat together egg, milk, salt, spices and vanilla. Heat a lightly oiled griddle or skillet over medium-high heat. Dunk each slice of bread in egg mixture, soaking both sides. Place in pan, and cook on both sides until golden. Serve hot."
+      },
+      {
+        
+        img: "mignon.jpg",
+        title: "Filet Mignon with Rich Balsamic Glaze",
+        ingredients: [
+          '2 filet mignon steaks',
+          '1/2 tsp of freshly ground black pepper',
+          'salt to taste',
+          '1/4 cup of balsamic vinegar',
+          '1/4 cup of dry red wine',
+        ],
+        instructions: "Sprinkle freshly ground pepper over both sides of each steak, and sprinkle with salt to taste. Heat a nonstick skillet over medium-high heat. Place steaks in hot pan, and cook for 1 minute on each side, or until browned. Reduce heat to medium-low, and add balsamic vinegar and red wine. Cover, and cook for 4 minutes on each side, basting with sauce when you turn the meat over. Remove steaks to two warmed plates, spoon one tablespoon of glaze over each, and serve immediately."
+      },
+      {
+        
+        img: "cookie.jpg",
+        title: "Best Chocolate Chip Cookie Ever!",
+        ingredients: [
+          '2 cups of all purpose flour',
+          '1/2 tsp of baking soda',
+          '1/2 tsp of salt',
+          '3/4 cup of unsalted butter, melted',
+          '1 cup of brown sugar',
+          '1/2 cup of white sugar',
+          '1 tbsp of vanilla extract',
+          '1 egg',
+          '1 egg yolk',
+          '2 cups of semisweet chocolate chips'
+        ],
+        instructions: "Preheat the oven to 325 degrees F (165 degrees C). Grease cookie sheets or line with parchment paper. Sift together the flour, baking soda and salt; set aside. In a medium bowl, cream together the melted butter, brown sugar and white sugar until well blended. Beat in the vanilla, egg, and egg yolk until light and creamy. Mix in the sifted ingredients until just blended. Stir in the chocolate chips by hand using a wooden spoon. Drop cookie dough 1/4 cup at a time onto the prepared cookie sheets. Cookies should be about 3 inches apart. Bake for 15 to 17 minutes in the preheated oven, or until the edges are lightly toasted. Cool on baking sheets for a few minutes before transferring to wire racks to cool completely."
+      }
+    ]
+  }
+  
   render() {
+    const recipe = this.props.recipes[2];
     return (
       <div className="App">
         <Recipe 
-          title="pasta" 
-          ingredients={['flour', 'water']}
-          instructions="Mix ingredients"
-          img="spaghetti.jpg"
+          title={recipe.title}
+          ingredients={recipe.ingredients}
+          instructions={recipe.instructions}
+          img={recipe.img}
           />
       </div>
     );
